0c2a76ad99b37acfa68b00b2727dacf3c73c5d82
YOUJUN <824000803@qq.com>
2022-03-01 14:25:11 +0800

    debug: modify config

--- web/src/utils/config.js
+++ web/src/utils/config.js
@@ -2,7 +2,7 @@ export default {
 
     devBaseUrl : 'http://localhost:3000',
     // prodBashUrl : 'http://106.13.63.236',
-    prodBashUrl : 'http://192.168.1.40:3000',
+    prodBashUrl : 'http://localhost:3000',
 
     baseUrl (){
         if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "devBuild"){
