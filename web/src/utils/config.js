533140a99f0b166fc0c9407feeab9efbd80c38bd
YOUJUN <824000803@qq.com>
2022-03-28 09:30:12 +0800

    feat: merge origin

--- web/src/utils/config.js
+++ web/src/utils/config.js
@@ -2,7 +2,7 @@ export default {
 
     devBaseUrl : 'http://localhost:3000',
     // prodBashUrl : 'http://106.13.63.236',
-    prodBashUrl : 'http://112.30.151.101:13000',
+    prodBashUrl : 'http://114.115.236.101:3000',
 
     baseUrl (){
         if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "devBuild"){
